666acb7ef002641c0e897c8006b3824aa580bc17
Vincenzo Cardea <129986281+vcardea@users.noreply.github.com>
2025-12-13 14:58:18 +0100

    Daje Roma daje
    Scritto insertProcQ, headProcQ, removeProcQ, outProcQ e aggiustato emptyChild, emptyProcQ, mkEmptyProcQ, freePcb con controlli per i parametri NULL.

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -8,17 +8,23 @@ static int next_pid = 1;
  * static array of MAXPROC PCBs. This method will be called only once during
  * data structure initialization. */
 // NOT TESTED
-void initPcbs() {
+void initPcbs()
+{
   INIT_LIST_HEAD(&pcbFree_h);
 
-  for (int i = 0; i < MAXPROC; i++) {
+  for (int i = 0; i < MAXPROC; i++)
+  {
     list_add(&pcbFree_table[i].p_list, &pcbFree_h);
   }
 }
 
 /* Insert the element pointed to by p onto the pcbFree list */
 // NOT TESTED
-void freePcb(pcb_t *p) { list_add(&p->p_list, &pcbFree_h); }
+void freePcb(pcb_t *p)
+{
+  if (p != NULL)
+    list_add(&p->p_list, &pcbFree_h);
+}
 
 /* Return NULL if the pcbFree list is empty. Otherwise, remove an element from
  * the pcbFree list, provide initial values for ALL of the PCBs fields (i.e.
@@ -28,10 +34,12 @@ void freePcb(pcb_t *p) { list_add(&p->p_list, &pcbFree_h); }
 
 /* Questa funzione prende un nodo dalla lista dei pcbFree_h(quelli non
  * utilizzati al momento) ne estrae uno e inizializza i suoi campi */
-pcb_t *allocPcb() {
+pcb_t *allocPcb()
+{
   if (list_empty(&pcbFree_h))
     return NULL;
-  else {
+  else
+  {
     // estraiamo il pcb a cui punta il nodo della lista che abbiamo eliminato
     // per poi inizializzarne i campi
     pcb_t *p = container_of(pcbFree_h.next, pcb_t, p_list);
@@ -63,49 +71,114 @@ pcb_t *allocPcb() {
 
 /* This method is used to
  * initialize a variable to be head pointer to a process queue. */
-// NOT TESTED
-void mkEmptyProcQ(struct list_head *head) { INIT_LIST_HEAD(head); }
+void mkEmptyProcQ(struct list_head *head)
+{
+  if (head != NULL)
+    INIT_LIST_HEAD(head);
+}
 
 /* return TRUE if the queue whose
  * head is pointed to by head is empty. Return FALSE otherwise */
-// NOT TESTED
-int emptyProcQ(struct list_head *head) { return list_empty(head); }
+int emptyProcQ(struct list_head *head)
+{
+  if (head == NULL)
+    return FALSE;
+  return list_empty(head);
+}
 
 /* Insert the PCB pointed by p into the process queue whose head pointer is
  * pointed to by head. The list must be ordered by priority. In case of equal
  * priority, the new PCB must be inserted after the last PCB with this priority.
  * For example, if you insert PCB N with priority 10 to the queue, A(20), B(10),
  * C(0), the list becomes A(20), B(10), N(10), C(0).*/
-// NOT TESTED
-void insertProcQ(struct list_head *head, pcb_t *p) {
-  pcb_t *n = container_of(head, struct pcb_t, p_list);
-  if (p->p_prio >= n->p_prio)
-    list_add(&p->p_list, head);
-  else
-    return insertProcQ(head->next, p);
+void insertProcQ(struct list_head *head, pcb_t *p)
+{
+  if (head == NULL || p == NULL)
+    return;
+
+  /*
+   * Itero su tutta la lista e ritorno non appena trovo il posto in cui mettere
+   * l'elemento (ovvero, quando la priorità dell'elemento corrente è minore di
+   * quello che sto cercando di inserire)
+   */
+  struct list_head *it;
+  list_for_each(it, head)
+  {
+    pcb_t *item = container_of(it, pcb_t, p_list);
+    if (p->p_prio > item->p_prio)
+    {
+      list_add_tail(&(p->p_list), it);
+      return;
+    }
+  }
+
+  /*
+   * Caso limite: se l'elemento che sto cercando di inserire è esattamente quello
+   * dalla priorità più bassa, allora aggiungo l'elemento in coda
+   */
+  list_add_tail(&(p->p_list), head);
 }
 
 /* Return a pointer to the first
  * PCB (i.e. the PCB with max priority) from the process queue whose head is
  * pointed to by head. Do not remove this PCB from the process queue. Return
  * NULL if the process queue is empty.*/
-pcb_t *headProcQ(struct list_head *head) {}
+pcb_t *headProcQ(struct list_head *head)
+{
+  if (head == NULL || emptyProcQ(head))
+    return NULL;
+  return container_of(head->next, pcb_t, p_list);
+}
 
 /* Return a pointer to the first PCB
  * (i.e. the PCB with max priority) from the process queue whose head is pointed
  * to by head. Return NULL if the process queue was initially empty; otherwise
  * return the pointer to the removed element.*/
-pcb_t *removeProcQ(struct list_head *head) {}
+pcb_t *removeProcQ(struct list_head *head)
+{
+  if (head == NULL || emptyProcQ(head))
+    return NULL;
+
+  pcb_t *item = container_of(head->next, pcb_t, p_list);
+  list_del(&(item->p_list));
+  return item;
+}
 
 /* Remove the PCB pointed to by p from the process queue whose head pointer is
  * pointed to by head. If the desired entry is not in the indicated queue (an
  * error condition), return NULL; otherwise, return p. Note that p can point to
  * any element of the process queue. */
-pcb_t *outProcQ(struct list_head *head, pcb_t *p) {}
+pcb_t *outProcQ(struct list_head *head, pcb_t *p)
+{
+  if (head == NULL || p == NULL)
+    return NULL;
+
+  /*
+   * Ricerca sequenziale dell'elemento da trovare
+   */
+  struct list_head *it;
+  list_for_each(it, head)
+  {
+    pcb_t *item = container_of(it, pcb_t, p_list);
+    if (item == p)
+    {
+      list_del(&(p->p_list));
+      return item;
+    }
+  }
+
+  // Elemento non presente nella coda
+  return NULL;
+}
 
 /* Return TRUE if the PCB pointed to by p has no
 children. Return FALSE otherwise. */
-int emptyChild(pcb_t *p) { return p->p_child.next == NULL; }
+int emptyChild(pcb_t *p)
+{
+  if (p == NULL)
+    return FALSE;
+  return p->p_child.next == NULL;
+}
 
 /* Make the PCB pointed to by p
  * a child of the PCB pointed to by prnt.*/
