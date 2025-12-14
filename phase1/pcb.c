8bf096c4c715f5da963d96a6efeffb9cf53c9844
Tostapane <79147922+Tostapane@users.noreply.github.com>
2025-12-14 13:57:22 +0100

    children

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -120,7 +120,7 @@ void insertProcQ(struct list_head *head, pcb_t *p)
 }
 
 /* Return a pointer to the first
- * PCB (i.e. the PCB with max priority) from the process queue whose head is
+* PCB (i.e. the PCB with max priority) from the process queue whose head is
  * pointed to by head. Do not remove this PCB from the process queue. Return
  * NULL if the process queue is empty.*/
 pcb_t *headProcQ(struct list_head *head)
@@ -150,7 +150,7 @@ pcb_t *removeProcQ(struct list_head *head)
  * any element of the process queue. */
 pcb_t *outProcQ(struct list_head *head, pcb_t *p)
 {
-  if (head == NULL || p == NULL)
+  if (head == NULL || p== NULL)
     return NULL;
 
   /*
@@ -177,20 +177,50 @@ int emptyChild(pcb_t *p)
 {
   if (p == NULL)
     return FALSE;
-  return p->p_child.next == NULL;
+  return list_empty(&(p -> p_child));
 }
 
 /* Make the PCB pointed to by p
  * a child of the PCB pointed to by prnt.*/
-void insertChild(pcb_t *prnt, pcb_t *p) {}
+void insertChild(pcb_t *prnt, pcb_t *p) 
+{
+     if (prnt == NULL || p == NULL || p -> p_parent != NULL)
+          return;
+     // aggiorno il puntatore di p al parent
+     p -> p_parent = prnt;
+     
+     // aggiungo p nella lista dei figli di parent
+     list_add_tail(&p -> p_sib, &prnt -> p_child);
+
+}
 
 /* Make the first child of the PCB pointed to by
  * p no longer a child of p. Return NULL if initially there were no children of
  * p. Otherwise, return a pointer to this removed first child PCB.*/
-pcb_t *removeChild(pcb_t *p) {}
+pcb_t *removeChild(pcb_t *p) 
+{
+     if (p == NULL || list_empty(&p -> p_child)) 
+          return NULL;
+     // accedo al figlio da rimuovere
+     pcb_t *item = container_of(p -> p_child.next, pcb_t, p_sib);
+
+     list_del(p -> p_child.next);
+     item -> p_parent = NULL;
+
+     return item;
+}
 
 /* Make the PCB pointed to by p no longer the
  * child of its parent. If the PCB pointed to by p has no parent, return NULL;
  * otherwise, return p. Note that the element pointed to by p could be in an
  * arbitrary position (i.e. not be the first child of its parent).*/
-pcb_t *outChild(pcb_t *p) {}
+pcb_t *outChild(pcb_t *p) 
+{
+     if (p == NULL || p -> p_parent == NULL)
+          return NULL;
+     // rimozione del figlio dalla lista dei fratelli
+     list_del(&p -> p_sib);
+     p -> p_parent = NULL;
+
+     return p;
+}
